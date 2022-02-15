13ae8b4c5782ff3b4d1b7b0ab60925e7d31aa481
stevtang <stevenmtang1@gmail.com>
2022-02-14 22:00:40 -0800

    show random pokemon

--- pokemon.js
+++ pokemon.js
@@ -2,12 +2,14 @@
 
 const BASE_URL = "https://pokeapi.co/api/v2/";
 const $pokeArea = $("#pokemon-area");
+const $pokeBtn = $("#get-mon-btn");
 const HEADERS = { 'content-type': 'application/json' };
 
 async function getMon() {
-    const monster = await axios.get(`${BASE_URL}pokemon/1`);
+    let randomMon = randomInteger(1,898);
+    const monster = await axios.get(`${BASE_URL}pokemon/${randomMon}`);
     console.log("mon: ", monster);
-    let monSrc = monster.data.sprites.front_default;
+    let monSrc = monster.data.sprites.front_shiny;
 
     $pokeArea.append(
       $("<img>", {
@@ -15,5 +17,9 @@ async function getMon() {
       }));
     return monster;
   }
+$pokeBtn.on("click", getMon);
+  // getMon();
 
-  getMon();
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
